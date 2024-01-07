a7d74e3b4c62053927ead29d4b9afe443853b2f2
Sunplix <sunpinghui2004@gmail.com>
2024-01-07 12:39:42 +0800

    v1.1

--- src/ui/LoginWindowUI.py
+++ src/ui/LoginWindowUI.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'ui.ui'
+## Form generated from reading UI file 'LoginWindowUI.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.6.0
 ##
@@ -18,7 +18,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
 from PySide6.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QLabel,
     QLineEdit, QMainWindow, QPushButton, QSizePolicy,
     QSpacerItem, QWidget)
-from . import images_rc
+from src.modules import images_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
